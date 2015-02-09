eb9820fe9aee82004e553f68cb3d5984cfa2bd50
Ekechi Ikenna <luccy559slim@gmail.com>
2015-02-09 10:56:46 +0100

    moving over to develop   Please enter the commit message for your changes. Lines starting

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150129142125) do
+ActiveRecord::Schema.define(version: 20150129071022) do
 
   create_table "comments", force: true do |t|
     t.integer  "user_id"
@@ -39,9 +39,9 @@ ActiveRecord::Schema.define(version: 20150129142125) do
     t.string   "name"
     t.text     "bio"
     t.string   "nickname"
+    t.string   "image"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "image"
   end
 
   create_table "likes", force: true do |t|
