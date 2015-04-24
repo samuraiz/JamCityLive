d4b5770718db085909c214bf2606e07b3c178582
andela-ajayeoba <dejijaye@gmail.com>
2015-04-24 23:10:07 +0100

    refactored voting system

--- app/models/contestant.rb
+++ app/models/contestant.rb
@@ -2,4 +2,7 @@ class Contestant < ActiveRecord::Base
   has_many :votes
   has_many :users, through: :votes, class_name: 'User'
   mount_uploader :image, ImageUploader
+  mount_uploader :image_b, ImageUploader
+  mount_uploader :image_c, ImageUploader
+  mount_uploader :image_d, ImageUploader
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,3 +11,4 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
 Rails.application.config.assets.precompile += %w( jquery.min.js )
 Rails.application.config.assets.precompile += %w( about.css )
+
