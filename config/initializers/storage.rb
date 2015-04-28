6149a87ddc71ce06c71052c1d345837758fbe03a
andela-ajayeoba <dejijaye@gmail.com>
2015-04-28 17:48:03 +0100

    change config vars

--- config/initializers/storage.rb
+++ config/initializers/storage.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
       provider:              'AWS',                        # required
-      aws_access_key_id:     "AKIAJ5CSME4SXJ2OSQVQ",                       # required
-      aws_secret_access_key: "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"                       # required
+      aws_access_key_id:     ENV["AWS_ACCESS_ID"],                       # required
+      aws_secret_access_key: ENV["AWS_ACCESS_SECRET_KEY"]                       # required
   }
   config.fog_directory  = "andela-ratsi"
   config.fog_public     = false                                        # optional, defaults to true
