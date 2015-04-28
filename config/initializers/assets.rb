e84b16dbd57c1e83796d225f993be37bcc9eea35
andela-ajayeoba <dejijaye@gmail.com>
2015-04-28 18:43:55 +0100

    refactor

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,5 +10,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.precompile += %w( bootstrap.min.css )
 Rails.application.config.assets.precompile += %w( bootstrap.min.js )
 Rails.application.config.assets.precompile += %w( jquery.min.js )
-Rails.application.config.assets.precompile += %w( about.css )
-
+Rails.application.config.assets.precompile += %w( application.css )
