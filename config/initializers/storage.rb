f5d670864c3a1f33465ffd645457473688b79402
andela-ajayeoba <dejijaye@gmail.com>
2015-04-28 17:59:15 +0100

    change deprecated file name

--- config/initializers/storage.rb
+++ config/initializers/storage.rb
@@ -1,10 +1,10 @@
 CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_credentials = {
-      provider:              'AWS',                        # required
-      aws_access_key_id:     ENV["AWS_ACCESS_ID"],                       # required
-      aws_secret_access_key: ENV["AWS_ACCESS_SECRET_KEY"]                       # required
+      provider:              'AWS',
+      aws_access_key_id:     ENV["AWS_ACCESS_ID"],
+      aws_secret_access_key: ENV["AWS_ACCESS_SECRET_KEY"]
   }
   config.fog_directory  = "andela-ratsi"
-  config.fog_public     = false                                        # optional, defaults to true
+  config.fog_public     = false
 end
