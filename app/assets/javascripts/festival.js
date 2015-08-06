76cca5bce70a1747844bb218709a5711b8294788
andela-ajayeoba <dejijaye@gmail.com>
2015-08-06 19:17:00 +0100

    changed festival dates

--- app/assets/javascripts/festival.js
+++ app/assets/javascripts/festival.js
@@ -1,6 +1,6 @@
 var getCountDown = function() {
     var clock = document.getElementById("countdown-holder")
-        , targetDate = new Date(2015, 07, 13); // Aug 13, 2015;
+        , targetDate = new Date(2015, 07, 28); // Aug 28, 2015;
 
     clock.innerHTML = countdown(targetDate).toString();
     setInterval(function(){
--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -8,34 +8,34 @@ class AboutController < ApplicationController
   end
 
   #sample codes for google drive: this just to push
-  def print_file(client, file_id)
-    drive = client.discovered_api('drive', 'v2')
-    result = client.execute(
-        :api_method => @drive.files.get,
-        :parameters => { 'fileId' => file_id })
-    if result.status == 200
-      file = result.data
-      puts "Title: #{file.title}"
-      puts "Description: #{file.description}"
-      puts "MIME type: #{file.mime_type}"
-    else
-      puts "An error occurred: #{result.data['error']['message']}"
-    end
-  end
-
-  def download_file(client, file)
-    if file.download_url
-      result = client.execute(:uri => file.download_url)
-      if result.status == 200
-        return result.body
-      else
-        puts "An error occurred: #{result.data['error']['message']}"
-        return nil
-      end
-    else
-      # The file doesn't have any content stored on Drive.
-      return nil
-    end
-  end
+  # def print_file(client, file_id)
+  #   drive = client.discovered_api('drive', 'v2')
+  #   result = client.execute(
+  #       :api_method => @drive.files.get,
+  #       :parameters => { 'fileId' => file_id })
+  #   if result.status == 200
+  #     file = result.data
+  #     puts "Title: #{file.title}"
+  #     puts "Description: #{file.description}"
+  #     puts "MIME type: #{file.mime_type}"
+  #   else
+  #     puts "An error occurred: #{result.data['error']['message']}"
+  #   end
+  # end
+  #
+  # def download_file(client, file)
+  #   if file.download_url
+  #     result = client.execute(:uri => file.download_url)
+  #     if result.status == 200
+  #       return result.body
+  #     else
+  #       puts "An error occurred: #{result.data['error']['message']}"
+  #       return nil
+  #     end
+  #   else
+  #     # The file doesn't have any content stored on Drive.
+  #     return nil
+  #   end
+  # end
 
 end
